43cefe33e062f30070cbbbb9e29b7f25ec93529d
Blvckhype <mateusz.lesiecki@gmail.com>
2018-10-13 12:12:17 +0200

    Added log4j with properties file

--- src/main/java/pracownia/introduction/Main.java
+++ src/main/java/pracownia/introduction/Main.java
@@ -1,8 +1,14 @@
 package pracownia.introduction;
 
+import org.apache.log4j.Logger;
+
 public class Main {
 
+    private static Logger logger = Logger.getLogger("myLogger");
+
     public static void main(String [ ] args) {
+
         System.out.println("Hello World");
     }
+
 }
